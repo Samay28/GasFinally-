30d30d48a04282d7ae7ebea9fb2d2748376d3cf3
Samay28 <imsamaystar@gmail.com>
2025-08-21 22:04:09 -0400

    Health bar added

--- Source/GasFinally/Private/HealthAttributeSet.cpp
+++ Source/GasFinally/Private/HealthAttributeSet.cpp
@@ -5,3 +5,30 @@ UHealthAttributeSet::UHealthAttributeSet()
 	InitHealth(100.0f);
 	InitMaxHealth(100.0f);
 }
+
+void UHealthAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT("PreChange: Attribute '%s'"), *Attribute.AttributeName);
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+	Super::PreAttributeBaseChange(Attribute, NewValue);
+}
+
+void UHealthAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+	UE_LOG(LogTemp, Warning, TEXT("PostChange: Attribute '%s' OldValue=%.1f NewValue=%.1f"), *Attribute.AttributeName, OldValue, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		OnHealthChanged.Broadcast(this, OldValue, NewValue);
+	}
+	else if (Attribute == GetMaxHealthAttribute())
+	{
+		const float CurrHealth = GetHealth();
+		OnHealthChanged.Broadcast(this, CurrHealth, CurrHealth);
+	}
+
+}
--- Source/GasFinally/Public/HealthAttributeSet.h
+++ Source/GasFinally/Public/HealthAttributeSet.h
@@ -7,9 +7,7 @@
 #include "AbilitySystemComponent.h"
 #include "HealthAttributeSet.generated.h"
 
-/**
- * 
- */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FAttributeChangedEvent, UAttributeSet*, AttributeSet, float, OldValue, float, NewValue);
 UCLASS()
 class GASFINALLY_API UHealthAttributeSet : public UAttributeSet
 {
@@ -26,5 +24,23 @@ class GASFINALLY_API UHealthAttributeSet : public UAttributeSet
 
 	ATTRIBUTE_ACCESSORS_BASIC(UHealthAttributeSet, Health);
 	ATTRIBUTE_ACCESSORS_BASIC(UHealthAttributeSet, MaxHealth);
-		
+
+	UFUNCTION(BlueprintPure, Category = "Attributes|Health")
+	float GetHealthCurrent() const { return Health.GetCurrentValue(); }
+
+	UFUNCTION(BlueprintPure, Category = "Attributes|Health")
+	float GetHealthBase() const { return Health.GetBaseValue(); }
+
+	UFUNCTION(BlueprintPure, Category = "Attributes|Health")
+	float GetMaxHealthCurrent() const { return MaxHealth.GetCurrentValue(); }
+
+	UFUNCTION(BlueprintPure, Category = "Attributes|Health")
+	float GetMaxHealthBase() const { return MaxHealth.GetBaseValue(); }
+
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override; // ourt attribute is laways clamped before it gets set
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override; // broadcast after the attribute has changed
+
+	UPROPERTY(BlueprintAssignable)
+	FAttributeChangedEvent OnHealthChanged;
 };
